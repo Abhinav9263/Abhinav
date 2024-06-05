cf230d7a5a6bd02b347912b5d4ec63e6508a4c86
Abhinav9263 <abhinav.bit121@gmail.com>
2024-06-05 13:22:40 +0530

    Max in 2D array.c

---
+++ Max in 2D array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int arr[rows][cols];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int max = arr[0][0];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (arr[i][j] > max) {
+                max = arr[i][j];
+            }
+        }
+    }
+
+    printf("The maximum element in the array is: %d\n", max);
+    return 0;
+}
